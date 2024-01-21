59ff455f4fba20c026c9170551004e07a0a9d525
RanaHamzaS <hamzathegreat110@gmail.com>
2024-01-21 14:37:12 +0500

    footer / new section added

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+function openPost(id) {
+    window.location.pathname = '/views/post.html'
+    window.localStorage.setItem('post_id', id);
+  }
+
+  fetch("https://dummyjson.com/posts")
+    .then((lockedData) => lockedData.json())
+    .then((data) => {
+      // Select the document element
+      const section2 = document.querySelector(".section-2");
+      section2.innerHTML = ''
+      for (let i = 0; i < data.posts.length; i++) {
+        console.log(data.posts[i])
+        section2.innerHTML += `
+        <div class="card" onclick="openPost(${data.posts[i].id})">
+            <div class="card-image"><img src="/assets/test-2.jpg" alt=""></div>
+            <div class="card-text">
+              ${data.posts[i].title}
+            </div>
+            <div class="card-items">
+              <span class="reactions">🩷${data.posts[i].reactions}</span>
+            </div>
+        </div>`;
+      }
+    });
+    fetch("https://dummyjson.com/posts")
+    .then((lockedData) => lockedData.json())
+    .then((data) => {
+      // Select the document element
+      const section3 = document.querySelector(".section-3");
+      section3.innerHTML = ''
+      for (let i = 0; i < data.posts.length; i++) {
+        console.log(data.posts[i])
+        section3    .innerHTML += `
+        <div class="card" onclick="openPost(${data.posts[i].id})">
+            <div class="card-image"><img src="/assets/test-2.jpg" alt=""></div>
+            <div class="card-text">
+              ${data.posts[i].title}
+            </div>
+            <div class="card-items">
+              <span class="reactions">🩷${data.posts[i].reactions}</span>
+            </div>
+        </div>`;
+      }
+    });
